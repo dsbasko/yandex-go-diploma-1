83da396c35018704d87539104f9baa3915ede646
Dmitriy Basenko <d.basenko.it@gmail.com>
2023-12-27 16:09:26 +0500

    feat: добавить prod сборку мигратора и описание под новую переменную

--- services/auth/migration.sh
+++ services/auth/migration.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export DSN="host=$AUTH_PSQL_CONTAINER_NAME port=${AUTH_PSQL_PORT} dbname=$AUTH_PSQL_DB user=$AUTH_PSQL_USER password=$AUTH_PSQL_PASS"
+export DSN="host=$PSQL_CONTAINER_NAME port=${PSQL_PORT} dbname=$PSQL_DB user=$PSQL_USER password=$PSQL_PASS"
 sleep 2 && goose -dir ./migrations postgres "${DSN}" up -v
